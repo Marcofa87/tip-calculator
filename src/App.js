0baf9f370a76bf63906f55951e79aee0249fb107
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-01-14 23:04:58 +0100

    Improved new features

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
   const [value, setValue] = useState(0);
   const [satisfaction, setSatisfaction] = useState(0);
   const [satisfaction2, setSatisfaction2] = useState(0);
-
+  const showPrice = value > 0;
   function handleReset() {
+    // eslint-disable-next-line no-restricted-globals
+    confirm("Are you sure to reset all the values?");
     setValue(0);
     setSatisfaction(0);
     setSatisfaction2(0);
@@ -27,11 +29,13 @@ function App() {
       >
         <p className="question">How did your friend like the service?</p>
       </Percentage>
-      <Price
-        value={value}
-        onSatisfaction={satisfaction}
-        onSatisfactionFriend={satisfaction2}
-      />
+      {showPrice && (
+        <Price
+          value={value}
+          onSatisfaction={satisfaction}
+          onSatisfactionFriend={satisfaction2}
+        />
+      )}
       <Reset onhandleReset={handleReset} />
     </div>
   );

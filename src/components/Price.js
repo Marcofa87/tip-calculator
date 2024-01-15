a754f3da8e63b29f47ee63cdee4fbf8a4a9ed690
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-01-15 17:10:33 +0100

    Removed parentheses

--- src/components/Price.js
+++ src/components/Price.js
@@ -5,7 +5,7 @@ export default function Price({ value, onSatisfaction, onSatisfactionFriend }) {
   return (
     <>
       <p className="price" style={tip > 10 ? { color: "blue" } : {}}>
-        You paid: ${totalAmount} ($({value}) + $({tip}) tip)
+        You paid: ${totalAmount} (${value} + ${tip} tip)
       </p>
     </>
   );
